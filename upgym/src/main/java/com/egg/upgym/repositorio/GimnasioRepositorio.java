68e3921c3dc9db07279172d2485b64eccd098a2d
Ezequiel-Olivera <82982891+Ezequiel-Olivera@users.noreply.github.com>
2021-06-15 18:48:21 -0300

    Update GimnasioRepositorio.java
    Ezequiel

--- upgym/src/main/java/com/egg/upgym/repositorio/GimnasioRepositorio.java
+++ upgym/src/main/java/com/egg/upgym/repositorio/GimnasioRepositorio.java
@@ -1,6 +1,18 @@
 package com.egg.upgym.repositorio;
 
+import com.egg.upgym.entidades.Gimnasio;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public class GimnasioRepositorio {
+@Repository
+public interface GimnasioRepositorio extends JpaRepository<Gimnasio, String> {
+    
+    @Query("SELECT g FROM Gimnasio g WHERE direccion.ciudad=:ciudad")
+    List<Gimnasio> buscarPorCiudad(@Param("ciudad")String ciudad); 
+    
+   
     
 }
