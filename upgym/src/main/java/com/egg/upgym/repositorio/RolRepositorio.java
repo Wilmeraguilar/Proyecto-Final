45799c889a160ee232472247f571a1fa39f2e60c
Ezequiel-Olivera <82982891+Ezequiel-Olivera@users.noreply.github.com>
2021-06-15 21:20:29 -0300

    Update RolRepositorio.java

--- upgym/src/main/java/com/egg/upgym/repositorio/RolRepositorio.java
+++ upgym/src/main/java/com/egg/upgym/repositorio/RolRepositorio.java
@@ -1,6 +1,10 @@
 package com.egg.upgym.repositorio;
 
+import com.egg.upgym.entidades.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class RolRepositorio {
+@Repository
+public interface RolRepositorio extends JpaRepository<Rol, String> {
     
 }
