7b32f107b91635448cf0f26815e8cabd48ac33c0
Ezequiel-Olivera <82982891+Ezequiel-Olivera@users.noreply.github.com>
2021-06-17 20:13:37 -0300

    update eze

--- upgym/src/main/java/com/egg/upgym/controlador/IndexControlador.java
+++ upgym/src/main/java/com/egg/upgym/controlador/IndexControlador.java
@@ -1,7 +1,24 @@
 
 package com.egg.upgym.controlador;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+@RequestMapping("/")
 public class IndexControlador {
+
+     @GetMapping("/")
+    public ModelAndView inicio(){
+        ModelAndView mav =new ModelAndView("inicio");
+        return mav;
+    }
+    
+    @GetMapping("/error-403")
+    public ModelAndView error403(){
+        return new ModelAndView("error403");
+    }
     
 }
