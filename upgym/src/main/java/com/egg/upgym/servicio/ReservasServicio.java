a741f87d4f103ea977e12e397686c2e835e456ce
GonzaloBaldiviezo <gonza.rb268@gmail.com>
2021-06-22 19:34:22 -0300

    update gonza 22-06-2021

--- upgym/src/main/java/com/egg/upgym/servicio/ReservasServicio.java
+++ upgym/src/main/java/com/egg/upgym/servicio/ReservasServicio.java
@@ -16,91 +16,102 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ReservasServicio {
-    
+
     @Autowired
     ReservasRepositorio resrep;
-    
+
     @Autowired
     UsuarioRepositorio usurep;
-    
+
     @Autowired
     GimnasioRepositorio gimrep;
-    
+
     @Transactional
-    public void crear(Date fecha, String horario, String idGimnasio,Long dniUsuario){
+    public void crear(Date fecha, String horario, String idGimnasio, Long dniUsuario) {
         Reservas reservas = new Reservas();
         Optional<Usuario> usuario = usurep.findById(dniUsuario);
         Optional<Gimnasio> gimnasio = gimrep.findById(idGimnasio);
-        
+
         Usuario u = usuario.get();
         Gimnasio g = gimnasio.get();
-        
-        reservas.setFecha(fecha);
-        reservas.setHorario(horario);
-        reservas.setGimnasio(g);
-        reservas.setUsuario(u);
 
-        resrep.save(reservas);
+        List<Reservas> listaCap = resrep.buscarPorGymHorarioFecha(idGimnasio, horario, fecha);
+
+        if (listaCap.size() < g.getCapacidad()) {
+            reservas.setFecha(fecha);
+            reservas.setHorario(horario);
+            reservas.setGimnasio(g);
+            reservas.setUsuario(u);
+
+            resrep.save(reservas);
+        } else {
+            System.out.println("La capacidad ha sido excedida");
+        }
     }
-    
-     @Transactional(readOnly = true)
+
+    @Transactional(readOnly = true)
     public Reservas buscarPorId(String id) {
         Optional<Reservas> reservaOptional = resrep.findById(id);
         return reservaOptional.orElse(null);
     }
-    
+
     @Transactional(readOnly = true)
-    public List<Reservas> buscarTodos(){
+    public List<Reservas> buscarTodos() {
         List<Reservas> reservas = resrep.findAll();
         return reservas;
     }
-    
-     @Transactional
+
+    @Transactional
     public void modificar(String id, Date fecha, String horario, String idGimnasio, Long idUsuario, String estado) {
 
         Optional<Reservas> reserva = resrep.findById(id);
         Optional<Usuario> usuario = usurep.findById(idUsuario);
         Optional<Gimnasio> gimnasio = gimrep.findById(idGimnasio);
-        
 
-        if (reserva.isPresent()) {
-            Reservas r = reserva.get();
-            Gimnasio g = gimnasio.get();
-            Usuario u = usuario.get();
+        List<Reservas> listaCap = resrep.buscarPorGymHorarioFecha(idGimnasio, horario, fecha);
 
-            if (r.getEstado().equalsIgnoreCase("ACTIVO")) {
-                r.setFecha(fecha);
-                r.setHorario(horario);
-                r.setGimnasio(g);
-                r.setUsuario(u);
-                r.setEstado(estado);
-                
-                resrep.save(r);
-            }else{
-                
-                System.out.println("La reserva se encuentra INACTIVA o TERMINADA. No se puede modificar");
-                
-            } 
+        if (listaCap.size() < gimnasio.get().getCapacidad()) {
+            if (reserva.isPresent()) {
+                Reservas r = reserva.get();
+                Gimnasio g = gimnasio.get();
+                Usuario u = usuario.get();
 
+                if (r.getEstado().equalsIgnoreCase("ACTIVO")) {
+                    r.setFecha(fecha);
+                    r.setHorario(horario);
+                    r.setGimnasio(g);
+                    r.setUsuario(u);
+                    r.setEstado(estado);
+
+                    resrep.save(r);
+                } else {
+
+                    System.out.println("La reserva se encuentra INACTIVA o TERMINADA. No se puede modificar");
+
+                }
+
+            }
+        } else {
+            System.out.println("La capacidad ha sido excedida");
         }
 
     }
-    
+
     @Transactional
     public void eliminar(String id) {
         Optional<Reservas> reserva = resrep.findById(id);
-        
+
         if (reserva.isPresent()) {
             Reservas r = reserva.get();
 
             if (r.getEstado().equalsIgnoreCase("ACTIVO")) {
                 r.setEstado("INACTIVO");
-                
+
                 resrep.save(r);
-            }else{
-                
+            } else {
+
                 System.out.println("La reserva se encuentra INACTIVA o TERMINADA. No se puede eliminar");
-                
+
             }
         }
     }
