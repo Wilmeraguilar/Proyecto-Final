f6c75921bd5622085c36c5743411fe5f20a154df
Ezequiel-Olivera <82982891+Ezequiel-Olivera@users.noreply.github.com>
2021-06-21 22:44:49 -0300

    Update ReservasRepositorio.java

--- upgym/src/main/java/com/egg/upgym/repositorio/ReservasRepositorio.java
+++ upgym/src/main/java/com/egg/upgym/repositorio/ReservasRepositorio.java
@@ -1,10 +1,14 @@
 package com.egg.upgym.repositorio;
 
 import com.egg.upgym.entidades.Reservas;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservasRepositorio extends JpaRepository<Reservas, String> {
-    
+    @Query("SELECT r FROM Reservas r WHERE gimnasio.id=:gimnasio AND horario=:horario")
+    List<Reservas> buscarPorGymYHorario(@Param("gimnasio") String gimnasio,@Param("horario") String horario);
 }
